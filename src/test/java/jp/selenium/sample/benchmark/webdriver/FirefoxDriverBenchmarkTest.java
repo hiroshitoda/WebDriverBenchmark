35da14275e047b5a3c5279c0e4028397d6066636
Hiroshi Toda <hiroshitoda@ossaaaan.com>
2016-10-09 19:27:56 +0900

    GeckoDriverに対応

--- src/test/java/jp/selenium/sample/benchmark/webdriver/FirefoxDriverBenchmarkTest.java
+++ src/test/java/jp/selenium/sample/benchmark/webdriver/FirefoxDriverBenchmarkTest.java
@@ -2,12 +2,18 @@ package jp.selenium.sample.benchmark.webdriver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.openqa.selenium.firefox.MarionetteDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class FirefoxDriverBenchmarkTest extends Base
 {
     @Override
     protected WebDriver getDriver() throws Exception
     {
-        return new FirefoxDriver();
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("useMarionette", true);
+        capabilities.setCapability(GeckoDriverService.GECKO_DRIVER_EXE_PROPERTY, "./geckodriver");
+        return new FirefoxDriver(capabilities);
     }
 }
